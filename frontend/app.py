ebedaf2e8702743ec8782c0874fdff733e97d41b
Gerard Kouonfo Njiti <47918869+kouong@users.noreply.github.com>
2025-11-05 11:27:09 +0100

    Change version number in home route response

--- frontend/app.py
+++ frontend/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello from EC2 via CodeDeploy!</h1><p>Version 2!</p>"
+    return "<h1>Hello from EC2 via CodeDeploy!</h1><p>Version 1</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
